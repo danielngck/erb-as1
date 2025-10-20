de1491f523de447be57f20750009c685505eaabe
danielngck <143630443+danielngck@users.noreply.github.com>
2025-10-20 23:00:27 +0800

    Add files via upload

---
+++ js/backtotop.js
@@ -0,0 +1,19 @@
+// Get the button
+const backToTopButton = document.getElementById("backToTop");
+
+// Show the button when scrolling down 100px from the top
+window.onscroll = function () {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+
+// Scroll to the top when the button is clicked
+backToTopButton.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" // Smooth scroll
+    });
+};
