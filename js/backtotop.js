ea1b755c3cb1542fe82defdefcb3a2b25f31e454
danielngck <143630443+danielngck@users.noreply.github.com>
2025-10-23 10:44:51 +0800

    Adjust scroll threshold for back to top button

--- js/backtotop.js
+++ js/backtotop.js
@@ -3,7 +3,7 @@ const backToTopButton = document.getElementById("backToTop");
 
 // Show the button when scrolling down 100px from the top
 window.onscroll = function () {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    if (document.body.scrollTop > 110 || document.documentElement.scrollTop > 110) {
         backToTopButton.style.display = "block";
     } else {
         backToTopButton.style.display = "none";
@@ -16,4 +16,5 @@ backToTopButton.onclick = function () {
         top: 0,
         behavior: "smooth" // Smooth scroll
     });
+
 };
